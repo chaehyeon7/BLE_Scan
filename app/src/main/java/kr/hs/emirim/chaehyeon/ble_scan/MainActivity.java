7ad15fd064636ab671540ec4f4adfde9e9f13a0e
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-09-16 23:10:27 +0900

    MainActivity.java Button add Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/ble_scan/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/ble_scan/MainActivity.java
@@ -1,14 +1,26 @@
 package kr.hs.emirim.chaehyeon.ble_scan;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    Button startButton;
+    Button stopButton;
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.start_button);
+        stopButton = findViewById(R.id.stop_button);
+        resetButton = findViewById(R.id.reset_button);
+
+
+        startButton.setOnClickListener(v -> start());
+
     }
 }
