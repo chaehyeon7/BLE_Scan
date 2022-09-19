7a10049f9fdb5b6b578d8d4ca2e1775ac38a4440
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-09-19 20:57:13 +0900

    MainActivity.java 블루투스, 버튼 활성화, 동작 구현

--- app/src/main/java/kr/hs/emirim/chaehyeon/ble_scan/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/ble_scan/MainActivity.java
@@ -1,14 +1,48 @@
 package kr.hs.emirim.chaehyeon.ble_scan;
 
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity{
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
 
-    Button startButton;
-    Button stopButton;
-    Button resetButton;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton; //시작버튼
+    Button stopButton; // 중지버튼
+    Button resetButton; // 초기화버튼
+
+    private ActivityResultLauncher<String[]> requestPermissionLauncher;
+    private ActivityResultLauncher<Intent> enableBluetoothLauncher;
+
+    private boolean hasCoarseLcationPermission = false;
+    private boolean hasFineLocationPermission = false;
+    private boolean hasBluetoothScanPermission = false;
+    private boolean hasBluetoothConnectPermission = false;
+
+    private BluetoothManager bluetoothManager;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothLeScanner bluetoothLeScanner;
+    private ScanCallback leScanCallback;
+
+    private boolean scanning = false;
+
+    private ListView listView;
+    private ListViewAdapter listViewAdapter;
+    private ArrayList<ScanResult> scanResultArrayList;
+
+    private ArrayList<String> mData = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +54,15 @@ public class MainActivity extends AppCompatActivity{
         resetButton = findViewById(R.id.reset_button);
 
 
+        startButton = findViewById(R.id.start_button);
+        stopButton = findViewById(R.id.stop_button);
+        resetButton = findViewById(R.id.reset_button);
+
         startButton.setOnClickListener(v -> start());
+        stopButton.setOnClickListener(v -> stop());
+        resetButton.setOnClickListener(v -> reset());
+
+
 
     }
 }
